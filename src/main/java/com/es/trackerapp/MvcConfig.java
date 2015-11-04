0446231f86b25693af1a2c994e306d533a26940c
Vikrant <chopra_vikrant@yahoo.com>
2015-11-04 13:56:28 +0530

    Initial Commit

---
+++ src/main/java/com/es/trackerapp/MvcConfig.java
@@ -0,0 +1,19 @@
+package com.es.trackerapp;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class MvcConfig extends WebMvcConfigurerAdapter {
+
+	@Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/home").setViewName("home");
+        registry.addViewController("/").setViewName("home");
+        registry.addViewController("/hello").setViewName("hello");
+        //registry.addViewController("/comments").setViewName("comments");
+        registry.addViewController("/login").setViewName("login");
+        //registry.addViewController("/signup").setViewName("signup");
+    }
+}
